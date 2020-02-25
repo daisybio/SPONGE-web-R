5c78e7ab62194d5f14d86ad280541d28d6ab604a
IceQueen1996 <50208793+IceQueen1996@users.noreply.github.com>
2020-02-25 13:18:39 +0100

    Update test_expression.R

--- tests/testthat/test_expression.R
+++ tests/testthat/test_expression.R
@@ -7,7 +7,6 @@ test_that("Gene Expression Values from API parsed correctly", {
   expect_error(get_geneExprValues(disease_name = "kidney clear cell carcinoma", gene_symbol = c("FooBar")), "API response is empty.*")
   expect_error(get_geneExprValues(disease_name = "kidney clear cell carcinoma", ensg_number = c("ENSG001bla"), gene_symbol = c("FooBar")), "More than one identification paramter is given.*")
   expect_error(get_geneExprValues(disease_name = NULL), "Required parameter disease_name is not given*")
-  expect_error(get_geneExprValues(), "argument \"disease_name\" is missing, with no default*")
 })
 
 test_that("miRNA Expression Values from API parsed correctly", {
@@ -16,5 +15,4 @@ test_that("miRNA Expression Values from API parsed correctly", {
   expect_error(get_mirnaExprValues(disease_name = "kidney clear cell carcinoma", hs_number = c("FooBar")), "API response is empty.*")
   expect_error(get_mirnaExprValues(disease_name = "kidney clear cell carcinoma", mimat_number = c("MIR-bla"), hs_number = c("FooBar")), "More than one identification paramter is given.*")
   expect_error(get_mirnaExprValues(disease_name = NULL), "Required parameter disease_name is not given*")
-  expect_error(get_mirnaExprValues(), "argument \"disease_name\" is missing, with no default*")
 })
